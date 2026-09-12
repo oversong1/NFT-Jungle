3b5c0e9e37fb5082ab74127542d4c8de4e7c9634
oversong1 <emersonsouza8@hotmail.com>
2026-09-12 13:13:25 -0300

    coloquei a parte de atualizar o server, estou usando o usePolling no vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
